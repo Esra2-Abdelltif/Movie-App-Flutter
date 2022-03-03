bfacf1262e0b84f2dc9a6df5ac4c568bbaae8166
Esra2-Abdelltif <esraa.abd.a99@gmail.com>
2022-03-03 21:03:52 +0200

    appBar

--- lib/HomeScreen/home_screen.dart
+++ lib/HomeScreen/home_screen.dart
@@ -6,7 +6,19 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(title: Text('Movie app'),) ,
+      backgroundColor: Colors.white,
+      appBar:AppBar(
+        title: Text('Movie app',
+          style: TextStyle(color: Colors.black87),
+      ),
+      shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.vertical(
+    bottom: Radius.circular(20),
+    ),
+      ),
+        elevation:5,
+        backgroundColor: Colors.white,
+        centerTitle: true,) ,
     );
   }
 }
